42fb84daa457051548a107254d87ee30022f6916
wearenotwhoiam <weareryanmeyers@gmail.com>
2025-05-17 20:37:36 -0700

    started gameplay tags

---
+++ Source/TinyWorldGameShow/Private/ContestantGameplayTags.cpp
@@ -0,0 +1,11 @@
+// RnDumb Fun - GameDev.TV Game Jam 2025
+
+
+#include "ContestantGameplayTags.h"
+
+namespace ContestantGameplayTags
+{
+	//**  Input Tags  **//
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+}
---
+++ Source/TinyWorldGameShow/Public/ContestantGameplayTags.h
@@ -0,0 +1,13 @@
+// RnDumb Fun - GameDev.TV Game Jam 2025
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace ContestantGameplayTags
+{
+	//**  Input Tags  **//
+	TINYWORLDGAMESHOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+		TINYWORLDGAMESHOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
